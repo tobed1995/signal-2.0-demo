faadb413c5f2939581ac3b108d8500cb55b481b9
Taha Obed <taha.obed@hotmail.de>
2023-11-05 08:26:57 +0100

    Initial Commit

---
+++ Signal 2.0 Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Signal 2.0 Demo
+//
+//  Created by Taha Obed on 05.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Signal 2.0 Demo/Signal_2_0_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Signal_2_0_DemoApp.swift
+//  Signal 2.0 Demo
+//
+//  Created by Taha Obed on 05.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Signal_2_0_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
